2a68deb6599e47d03d7b2f05652a9d7fd17d2aaa
Victor <victorcarnevale@yahoo.com.br>
2021-05-14 15:55:14 -0300

    CategoryResource

---
+++ Backend/src/main/java/com/vfornazi/dscatalog/resources/CategoryResource.java
@@ -0,0 +1,28 @@
+package com.vfornazi.dscatalog.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vfornazi.dscatalog.entity.Category;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+	
+	
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll () {
+		List<Category> list = new ArrayList<>();
+		
+		list.add(new Category(1l, "Books"));
+		list.add(new Category(2l, "Electronics"));
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+}
